6789c31f46d9ff09f9ea143ad250e1766939a363
Jirasak07 <bee.110243@gmail.com>
2022-11-16 13:02:47 +0700

    ...

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
-const mysql = require('mysql2')
+var express = require("express");
+var cors = require("cors");
+var app = express();
+const mysql = require("mysql2");
 
-const connection =mysql.createConnection({
-    host:'localhost'
-    ,user:'root',
-    database:'equipmentdb'
-})
-app.use(cors())
-app.get('/',function(req,res,next){
-    res.json('hi this is API')
-})
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "equipmentdb",
+});
+app.use(cors());
+app.get("/", function (req, res, next) {
+  res.json("hi this is API");
+});
 
-app.listen(5409,function(){
-    console.log('CORS PORT 85')
-})
+app.listen(2222, function () {
+  console.log("CORS PORT 85");
+});
