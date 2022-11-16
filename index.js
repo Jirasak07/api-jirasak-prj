123c401d7325b674aa5d570e17dc76c4141155d1
Jirasak07 <bee.110243@gmail.com>
2022-11-16 17:05:44 +0700

    connect sqlsvr

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 var express = require("express");
 var cors = require("cors");
 var app = express();
-const mysql = require("mysql2");
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+  var sql = require("mssql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "equipmentdb",
-});
+ // config for your database
+  var config = {
+    user: "Merlin",
+    password: "Jirasak5409",
+    server: "jirasak.database.windows.net",
+    database: "jirasakdb",
+  };
+
 app.use(cors());
 app.get("/", function (req, res, next) {
   res.json("hi this is API");
 });
+app.get("/test",jsonParser, function (req, res) {
+  // connect to your database
+  sql.connect(config, function (err) {
+    if (err) console.log(err);
 
+    // create Request object
+    var request = new sql.Request();
+
+    // query to the database and get the records
+    request.query("select * from dbo.product", function (err, results) {
+      if (err) console.log(err);
+
+      // send records as a response
+      res.send(results.recordset);
+      console.log(results.recordset)
+    });
+  });
+});
 app.listen(2222, function () {
   console.log("CORS PORT 85");
 });
+
+//////////////////////
